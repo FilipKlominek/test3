e8fb61a9c38dafffafb83264e332f66d219fb7ce
FilipKlominek <71869121+FilipKlominek@users.noreply.github.com>
2021-09-23 15:59:08 +0200

    Add files via upload

--- test7/src/com/company/Main.java
+++ test7/src/com/company/Main.java
@@ -12,25 +12,26 @@ public class Main {
             int currentAmount;
             int total = 0;
 
+            int[] banknotes = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+            int[] notesUsed = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
             while ((line = br.readLine()) != null) {
                 currentAmount = Integer.parseInt(line);
                 while (currentAmount > 0) {
                     total++;
-                    if (currentAmount >= 5000) currentAmount -= 5000;
-                    else if (currentAmount >= 2000) currentAmount -= 2000;
-                    else if (currentAmount >= 1000) currentAmount -= 1000;
-                    else if (currentAmount >= 500) currentAmount -= 500;
-                    else if (currentAmount >= 200) currentAmount -= 200;
-                    else if (currentAmount >= 100) currentAmount -= 100;
-                    else if (currentAmount >= 50) currentAmount -= 50;
-                    else if (currentAmount >= 20) currentAmount -= 20;
-                    else if (currentAmount >= 10) currentAmount -= 10;
-                    else if (currentAmount >= 5) currentAmount -= 5;
-                    else if (currentAmount >= 2) currentAmount -= 2;
-                    else currentAmount -= 1;
+                    for (int i = 0; i < banknotes.length; i++) {
+                        if (currentAmount >= banknotes[i]) {
+                            currentAmount -= banknotes[i];
+                            notesUsed[i]++;
+                            break;
+                        }
+                    }
                 }
             }
-            System.out.println(total);
+            System.out.println("total: " + total);
+            for (int i = 0; i < notesUsed.length; i++) {
+                System.out.println(banknotes[i] + ": " + notesUsed[i]);
+            }
 
         } catch (Exception e) {
             System.out.println("not found");
